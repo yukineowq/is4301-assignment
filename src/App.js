aff4d51004d5388fd0f6b2532e8fa69235d36685
Yuki <yuki.neo34@gmail.com>
2022-10-20 02:59:32 +0800

    added imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import {
   BrowserRouter as Router, Link} from "react-router-dom";
+import Home from "./pages/Home.js";
+import Academics from "./pages/Academics.js";
+import Admission from "./pages/Admission.js";
 
 function App() {
   return (
