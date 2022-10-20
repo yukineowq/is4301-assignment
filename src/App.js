a71e966f11bf919834c3ee024676dfe96f9f903e
Yuki <yuki.neo34@gmail.com>
2022-10-20 14:34:31 +0800

    added a form pulgin for the login/register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import {
-  BrowserRouter as Router, Link} from "react-router-dom";
-import Home from "./pages/Home.js";
-import Academics from "./pages/Academics.js";
-import Admission from "./pages/Admission.js";
+import { BrowserRouter as Router, Link} from "react-router-dom";
 
 function App() {
   return (
